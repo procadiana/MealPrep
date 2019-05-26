5200fd3b04ce1a1eea1afbe4f7e85740c15f72bf
karinalop <karina.popeye@gmail.com>
2019-05-26 17:06:38 +0000

    Test conexion edeman api

--- rails-back-end/app/controllers/application_controller.rb
+++ rails-back-end/app/controllers/application_controller.rb
@@ -3,16 +3,25 @@ class ApplicationController < ActionController::API
   include HTTParty
 
 
-  BASE_URL = "https://api.edamam.com/search"
-  API_PARTIAL_URL = "&app_id=#{ENV['KEY_API_ID']}" + "&" +"app_key=#{ENV['KEY_API_PASS']}"
-  query = BASE_URL+ API_PARTIAL_URL + "&q=chicken"
-  q = "https://api.edamam.com/search?q=chicken#{API_PARTIAL_URL}&time=10-30"
-  puts q
-  #a = "https://api.edamam.com/search?q=chicken&q=cucumber&app_id=1404bd0e&app_key=ad0ecd1c212bcb688d6462d5dda1bfb1&time=10-30&yield=3&health=peanut-free"
-  request = HTTParty.get(q).to_json
-  request_hash = JSON.parse(request)
-  puts request_hash
+  BASE_URL = "https://api.edamam.com/search?"
+  API_PARTIAL_URL = "app_id=#{ENV['KEY_API_ID']}" + "&" +"app_key=#{ENV['KEY_API_PASS']}"
+  #query = BASE_URL+ API_PARTIAL_URL + "&q=chicken"
+  #q = "https://api.edamam.com/search?q=chicken#{API_PARTIAL_URL}&time=10-30"
+  #puts q
+  # request = HTTParty.get(q).to_json
+  # request_hash = JSON.parse(request)
+  # test_r = request_hash["hits"][0]
+  # puts r = test_r["recipe"]["uri"] #http://www.edamam.com/ontologies/edamam.owl#recipe_9a7c1c4c891c72f649746650a9635919
+  #r=http%3A%2F%2Fwww.edamam.com%2Fontologies%2Fedamam.owl%23recipe_9b5945e03f05acbf9d69625138385408
+  #query_recipe = BASE_URL+ API_PARTIAL_URL + "&r=http%3A%2F%2Fwww.edamam.com%2Fontologies%2Fedamam.owl%23recipe_9a7c1c4c891c72f649746650a9635919"
+  #"&r=#{r}"
+  #puts query_recipe = "https://api.edamam.com/search?app_id=4377435e&app_key=ff344f7fcaf17dfc1eb3b37cfbeac61a&r=http%3A%2F%2Fwww.edamam.com%2Fontologies%2Fedamam.owl%23recipe_9a7c1c4c891c72f649746650a9635919"
 
-  #https://api.edamam.com/search?q=chicken&q=cucumber&app_id=1404bd0e&app_key=ad0ecd1c212bcb688d6462d5dda1bfb1&time=10-30&yield=3&health=peanut-free
+  #r_recipe= HTTParty.get(query_recipe)
+  #puts "hello #{r_recipe}"
+  #puts JSON.parse(r_recipe)
+  # def query(query_string)
+
+  # end
 
 end
