f949c055aab2ce2476a1f80d14b1c624680e0da6
procadiana <diana.claudia.ilinca@gmail.com>
2019-05-29 00:37:09 +0000

    Styling

--- react-front-end/src/Recipe.jsx
+++ react-front-end/src/Recipe.jsx
@@ -33,7 +33,7 @@ export default class Recipe extends Component {
           type: "success",
           message: "Your recipe was added to favourites.",
           insert: "top",
-          container: "top-right",
+          container: "top-center",
           animationIn: ["animated", "fadeIn"],
           animationOut: ["animated", "fadeOut"],
           dismiss: { duration: 500 },
@@ -43,6 +43,7 @@ export default class Recipe extends Component {
         favourite: true
       })
     }
+    //insert get route to axios
   }
 
 
@@ -55,7 +56,7 @@ export default class Recipe extends Component {
 
         <li key={item['id']} style={{display: 'inline-block', margin:'15px'}} >
         <ReactNotification ref={this.notificationDOMRef} />
-            <img href={item['shareAs']} src={item['img_source']} style={{width:150}} />
+            <a href={item['shareAs']}><img  src={item['img_source']} style={{width:150}} /></a>
             <p><a href={item['shareAs']}>{item['name']}</a></p>
             <span style={{display: 'inline', padding:'5px'}}><FontAwesomeIcon icon="heart" href='' onClick={this.addNotification} style={{color: this.state.favourite ? 'red': 'black'}}  />  &nbsp;&nbsp; &nbsp;&nbsp;    <FontAwesomeIcon icon="times" href='' />  &nbsp;&nbsp;&nbsp;&nbsp;    <FontAwesomeIcon icon="check" href='' />  &nbsp;&nbsp;   </span>
         </li>
