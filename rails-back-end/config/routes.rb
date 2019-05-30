1608f6ade8862af2258d83020dd2472efeee93f8
karinalop <karina.popeye@gmail.com>
2019-05-30 18:12:26 +0000

    Add authentication functionality

--- rails-back-end/config/routes.rb
+++ rails-back-end/config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  post 'api/login' => 'api/sessions#create'
-  get 'api/logout' => 'api/sessions#destroy'
+  post '/api/login' => 'api/sessions#create'
+  get '/api/logout' => 'api/sessions#destroy'
 
 
   namespace :api do
