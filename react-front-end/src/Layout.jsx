25ac9ed1351377a94a5e5d20bc7ba0924626315e
anithaamarnath <anithaamarnath04@gmail.com>
2019-05-30 19:28:40 +0000

    user profile added in setting

--- react-front-end/src/Layout.jsx
+++ react-front-end/src/Layout.jsx
@@ -4,6 +4,8 @@ import './styles/homepage.css';
 import logo from './img/Logo2.png';
 import login from './Login.jsx';
 import HomePage from './Homepage.jsx';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 export default class Layout extends Component {
   render() {
@@ -30,8 +32,8 @@ export default class Layout extends Component {
 
                 </NavItem>
                 <UncontrolledDropdown setActiveFromChild className="nav_bar_name">
-                <DropdownToggle tag="a" className="nav-link" caret>Setting </DropdownToggle>
-                <DropdownMenu >
+                <DropdownToggle tag="a" className="nav-link" caret>  <FontAwesomeIcon icon="user" href=''/> Setting </DropdownToggle>
+                <DropdownMenu>
                 <DropdownItem tag="a" href="/blah" active>Edit User</DropdownItem>
                 <DropdownItem tag="a" href="/blah" active>Edit Meal Plan</DropdownItem>
                 <DropdownItem tag="a" href="/" style={{color:'#e33d26'}} active>Logout</DropdownItem>
