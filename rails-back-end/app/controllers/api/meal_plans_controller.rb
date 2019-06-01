d60bfd6a4808af79aaa45e8b49557a8e68fdc293
karinalop <karina.popeye@gmail.com>
2019-06-01 22:25:16 +0000

    Add shoping list

--- rails-back-end/app/controllers/api/meal_plans_controller.rb
+++ rails-back-end/app/controllers/api/meal_plans_controller.rb
@@ -23,11 +23,12 @@ class Api::MealPlansController < ApplicationController
   #if current_user
    mealplan = MealPlan.find(params[:id])
    recipes = mealplan.recipes
-   ingredients = list_of_ingredients(recipes)
+   #ingredients = list_of_ingredients(recipes)
+   shopping_list = shopping_list(recipes)
      render json: {
        mealplan: mealplan,
        recipes: recipes,
-       ingredients: ingredients
+       ingredients: shopping_list
       }
   #   else
   #     puts "user not allowed to see this mealplan"
@@ -38,11 +39,12 @@ class Api::MealPlansController < ApplicationController
     #if current_user
       mealplan = MealPlan.last #.where(user_id: session[:user_id])
       recipes = mealplan.recipes
-      ingredients = list_of_ingredients(recipes)
+      #ingredients = list_of_ingredients(recipes)
+      shopping_list = shopping_list(recipes)
       render json: {
        mealplan: mealplan,
        recipes: recipes,
-       ingredients: ingredients
+       ingredients: shopping_list
       }
     #end
   end
@@ -72,26 +74,58 @@ class Api::MealPlansController < ApplicationController
 
  private
 
- def list_of_ingredients (recipes)
-   q_string = "#{BASE_URL}&#{API_PARTIAL_URL}"
-   recipes.each { |r|
-     q_string = q_string + "&" + RECIPE_PARTIAL_URL + r.edaman_id
-   }
-   puts q_string
-   result = HTTParty.get(q_string)
-   list = []
+ # def list_of_ingredients (recipes)
+ #   q_string = "#{BASE_URL}&#{API_PARTIAL_URL}"
+ #   recipes.each { |r|
+ #     q_string = q_string + "&" + RECIPE_PARTIAL_URL + r.edaman_id
+ #   }
+ #   puts q_string
+ #   result = HTTParty.get(q_string)
+ #   list = []
 
-   if result
-     result.each { |r|
-       list = list + r["ingredientLines"]
+ #   if result
+ #     result.each { |r|
+ #       list = list + r["ingredientLines"]
+ #     }
+ #   else
+ #    puts result
+ #  end
+ #    puts "List of ingredients: #{list}"
+ #    return list
+
+ # end
+
+  def shopping_list(recipes)
+    q_string = "#{BASE_URL}&#{API_PARTIAL_URL}"
+     recipes.each { |r|
+       q_string = q_string + "&" + RECIPE_PARTIAL_URL + r.edaman_id
      }
-   else
-    puts result
-  end
-    puts "List of ingredients: #{list}"
-    return list
+     puts q_string
+     result = HTTParty.get(q_string)
+     ing =[]
+     shopping_list_hash = {}
+     shopping_list_array =[]
 
- end
+    if result
+      result.each { |r|
+        ing = ing + r["ingredients"]
+      }
+    end
+    puts ing
+    ing.each { |i|
+        if !shopping_list_hash[i['food']]
+          shopping_list_hash[i['food']] = i['weight']
+        else
+          shopping_list_hash[i['food']] = shopping_list_hash[i['food']] + i['weight']
+        end
+      }
+    # converting the hash into an array
+    shopping_list_hash.each do |key, value|
+      v = "#{key} - #{value.round(2).to_s} g"
+      shopping_list_array.push(v)
+    end
+    return shopping_list_array
+  end
 
 end
 
