a862c2dfcc764ebb3d02f1fc0bf6e32d241bfe4b
anithaamarnath <anithaamarnath04@gmail.com>
2019-06-01 02:34:13 +0000

    recipe remove functionality

--- react-front-end/src/Recipe.jsx
+++ react-front-end/src/Recipe.jsx
@@ -1,11 +1,8 @@
 import React, {Component} from 'react';
 import {Button,Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import axios from "axios";
-
 import ReactNotification from "react-notifications-component";
 import "react-notifications-component/dist/theme.css";
-
-
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -19,7 +16,9 @@ export default class Recipe extends Component {
 
       };
     this.addNotification = this.addNotification.bind(this);
+    this.removeNotification = this.removeNotification.bind(this);
     this.notificationDOMRef = React.createRef();
+
   }
 
 
@@ -50,6 +49,33 @@ export default class Recipe extends Component {
       }
   }
 
+  removeNotification() {
+    if (this.state.remove){
+      this.setState({
+        remove: true
+      })
+      }else {
+        this.notificationDOMRef.current.addNotification({
+          type: "danger",
+          message: "Your recipe was removed from favourites.",
+          insert: "top",
+          container: "top-center",
+          animationIn: ["animated", "fadeIn"],
+          animationOut: ["animated", "fadeOut"],
+          dismiss: { duration: 500 },
+          dismissable: { click: true }
+        });
+
+         axios.post(`/api/recipe/${this.props.recipe.id}`)
+          .then((response) =>{
+            remove: false
+
+          })
+        })
+      }
+
+  }
+
   // isFavourite = () =>
   //   axios.get(`/api/recipe/${this.props.recipe.id}`).then(response =>{
   // }
@@ -61,11 +87,14 @@ export default class Recipe extends Component {
     return (
         <>
 
-        <li key={item['id']} style={{display: 'inline-block', margin:'15px'}} >
+        <li key={item['id']} className ="recipe_display">
         <ReactNotification ref={this.notificationDOMRef} />
-            <a href={item['shareAs']}><img  src={item['img_source']} style={{width:150}} /></a>
+            <a href={item['shareAs']}><img  src={item['img_source']} className= "recipe_item" /></a>
             <p><a href={item['shareAs']}>{item['name']}</a></p>
-            <span style={{display: 'inline', padding:'5px'}}><FontAwesomeIcon icon="heart" href='' onClick={this.addNotification} style={{color: this.state.favourite ? 'red': 'black'}}  />  &nbsp;&nbsp; &nbsp;&nbsp;    <FontAwesomeIcon icon="times" href='' />  &nbsp;&nbsp;&nbsp;&nbsp;    <FontAwesomeIcon icon="check" href='' />  &nbsp;&nbsp;   </span>
+            <span style={{display: 'inline', padding:'5px'}}>
+            <FontAwesomeIcon icon="heart" href='' onClick={this.addNotification} style={{color: this.state.favourite  ? 'red': 'black'}}  />  &nbsp;&nbsp; &nbsp;&nbsp;
+            <FontAwesomeIcon icon="times" href='' onClick={this.removeNotification} style={{color: this.state.remove ? 'red': 'black'}}  />  &nbsp;&nbsp;&nbsp;&nbsp;
+            <FontAwesomeIcon icon="check" href='' />  &nbsp;&nbsp;   </span>
         </li>
         </>
       )
